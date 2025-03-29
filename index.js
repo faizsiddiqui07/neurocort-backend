59c6a97c642bf976f0539de16e38cdf4ca016cf2
Mohd Faiz <mohdfaiz.1842@gmail.com>
2025-03-29 10:08:21 +0530

    add www

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 });
 
 app.use(cors({
-    origin: ["http://localhost:5174", "http://localhost:5173","https://neurocort.com"],
+    origin: ["http://localhost:5174", "http://localhost:5173","https://neurocort.com","https://www.neurocort.com"],
     credentials: true,
 }))
 
